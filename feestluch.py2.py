4545c50a9b49e1e269f11e11d23346c8e8fa2786
Sam hulie <99065436@mydavinci.nl>
2022-09-08 12:49:00 +0200

    person.py

---
+++ feestluch.py2.py
@@ -0,0 +1,14 @@
+from re import T
+
+
+croissantjes = int(input ('Hoeveel croissantjes wilt u?'))
+croistotaal = croissantjes * 0.39
+stokbrood = int(input("hoeveel stokbroden wilt u?"))
+stokbrTotaal = stokbrood * 2.78
+aantal_kortingsbonnen = int(input (" (Hoeveel kortingsbonnen heeft u?"))
+korting = int(input ("hoeveel cent zijn ze waard?"))
+korting_totaal = aantal_kortingsbonnen * korting / 100
+
+totaal= stokbrTotaal + croistotaal - korting_totaal
+
+print(f"De feestlunch kost je bij de bakker {totaal} euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!")
---
+++ speelhal.py2.py
@@ -0,0 +1,18 @@
+ticket = 7.45
+ticketkosten = ticket * int(input("Met hoeveel personen wilt u naar de gamehal?"))
+
+
+virtualrealitypp = int(input ("hoeveel minuten wilt VRen?") )
+kosten_per_minuut = (0.074)
+kosten_vr = virtualrealitypp * kosten_per_minuut
+
+kosten_vr_AF = round (kosten_vr,2)
+print('Virtual reality kosten per persoon: ',kosten_vr_AF)
+totalvrcost = kosten_vr_AF * 4 
+print("Totale kosten Virtual reality:", {totalvrcost})
+
+print("Kosten toegang gamehall: ", ticketkosten)
+TotalCost = totalvrcost + ticketkosten
+afgrondtotaal= round (TotalCost,2)
+print("de totalen kosten voor de gamehall is ", {afgrondtotaal})
+print("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar" ,{afgrondtotaal})
