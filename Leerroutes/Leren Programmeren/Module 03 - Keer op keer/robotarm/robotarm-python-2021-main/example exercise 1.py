b74f2979050d6a31641dec039a94ce3acfb40780
Sam hulie <99065436@mydavinci.nl>
2022-11-08 01:18:22 +0100

    keeropkeer

--- Leerroutes/Leren Programmeren/Module 03 - Keer op keer/robotarm/robotarm-python-2021-main/example exercise 1.py
+++ Leerroutes/Leren Programmeren/Module 03 - Keer op keer/robotarm/robotarm-python-2021-main/example exercise 1.py
@@ -1,15 +1,12 @@
 from RobotArm import RobotArm
 
-robotArm = RobotArm('exercise 1')
+robotArm = RobotArm('exercise 6')
 # robotArm.moveLeft
 # robotArm.moveRight
 # robotArm.grab
 # robotArm.drop
 
 # Jouw python instructies zet je vanaf hier:
-robotArm.moveRight()
-robotArm.grab()
-robotArm.moveLeft()
-robotArm.drop()
+robotArm.moveRight
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
