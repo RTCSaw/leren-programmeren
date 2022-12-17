367f352f98780332d430059c4b732adf0f44fd6a
Sam hulie <99065436@mydavinci.nl>
2022-12-17 11:35:10 +0100

    module 5

--- Leerroutes/Leren Programmeren/Module 04 - Lijstjes en samenstellingen/lootjes.py
+++ Leerroutes/Leren Programmeren/Module 04 - Lijstjes en samenstellingen/lootjes.py
@@ -1,35 +1,29 @@
 import random
-namenlijst = []
-namenlijst_shuffle = []
-namen = 0
-while namen<3:
-    naam_input = input("Welke naam wilt u toevoegen?")
-    if naam_input not in namenlijst:
-        namenlijst.append(naam_input)
-        namenlijst_shuffle.append(naam_input)
-        namen +=1
+
+# de if controleerd voor een minimale input van 3
+while True:
+    aantal_deelnemers = int(input('Hoeveel namen wilt u toevoegen? : '))
+    if aantal_deelnemers <3:
+        print("u moet 3 of meer namen invullen")
     else:
-        print("Deze naam heeft u al toegevoegd.")
-    while namen >= 3:
-        nog_een_naam = input("Wilt u nog een naam toevoegen?")
-        if nog_een_naam =="ja":
-                naam_input = input("Welke naam wilt u toevoegen?")
-        else:
-            break
-        if naam_input not in namenlijst:
-            namenlijst.append(naam_input)
-            namenlijst_shuffle.append(naam_input)
-        else:
-            print("Deze naam heeft u al toegevoegd.")
+        break
+# input van namen, kijkt of de namen uniek zijn en voegt ze te toe aan de namen lijst.
+namen = []
+teller = 0 
+for x in range(aantal_deelnemers):
+    naam_input = input(f'Voeg de naam van deelnemer {teller + 1} toe: ')
+    if naam_input not in namen:
+        namen.append(naam_input)
+        teller +=1 
+    else:
+        print(" deze namen zit al in de lijst...")
 
+# hier wordt de namen lijstt geshuffeld
+random.shuffle(namen)
 
-for x in namenlijst_shuffle:
-    random.shuffle(namenlijst_shuffle)
-    for x in namenlijst_shuffle:
-            print("test")
-print(namenlijst)
-print(namenlijst_shuffle)
+#de zip functie maakt van koppels een lijst en zip creeert hier een koppel tussen de namen lootjs met een index zodat
+koppels = list(zip(namen, namen[1:] + [namen[0]]))
 
-
-
-
+# Print uit de koppels
+for gever, ontvanger in koppels:
+    print(f'{gever} heeft {ontvanger} lootje ')
---
+++ Leerroutes/Leren Programmeren/Module 05 - Meer Fun(ctions)/hello.py
@@ -0,0 +1,4 @@
+def greet():
+    print("hello from function town")
+
+greet()
