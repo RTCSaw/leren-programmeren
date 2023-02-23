ff2768b12a915aaea70928a82487da217fda4ae3
Sam hulie <99065436@mydavinci.nl>
2023-02-23 09:45:21 +0100

    Java uitdaging

---
+++ Leerroutes/Leren Programmeren/Java Script/dieren.js
@@ -0,0 +1,15 @@
+function aantal_dieren ()
+{
+    let aantal_dieren = prompt("hoeveel dieren?");
+    dieren = {giraffe : { poten : 4}, struisvogel : {poten : 2 }, zebra : {poten : 4}}
+    for (['dier'] = 0; [poten] = poten ; ++1) {
+        totaal = aantal_poten * ['poten']
+      }
+    
+    console.log(totaal)
+};
+aantal_dieren()
+
+
+
+
