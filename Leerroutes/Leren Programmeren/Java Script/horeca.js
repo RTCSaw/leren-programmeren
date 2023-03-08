c504f20587555ef5176d8cb60fd519165fbedb33
Sam hulie <99065436@mydavinci.nl>
2023-03-08 12:14:42 +0100

    horeca

--- Leerroutes/Leren Programmeren/Java Script/horeca.js
+++ Leerroutes/Leren Programmeren/Java Script/horeca.js
@@ -1,23 +1,28 @@
-let boodschappen = {};
+
 loop = true
+var drank ={"bier": {name: "bier",price: 2.50, amount:0}, "wijn": {name: "wijn",price: 2.80, amount:0}, "fris": {name: "fris",price: 2.05, amount:0}}
+
 while (loop) {
     let drinken = prompt("Wat wilt u bestellen?");
-    if(drinken == "bier" || drinken =="wijn" || drinken =="fris" ){
+    if(drinken in drank){
         console.log("dit ken ik")
         let aantal = prompt("Hoeveel "+ drinken +" wilt u?");
-        boodschappen[drinken]= aantal
-
+        drank[drinken].amount += parseInt(aantal);
     }else if (drinken == "stop"){
         loop = false 
-    }
-    else if (drinken in boodschappen){
-        boodschappen[drinken]+= aantal
     }
     else{
         console.log("dit ken ik niet... probeer iets anders")
     }
 };
-for (key in boodschappen){
-    console.log(boodschappen[key])
-    document.getElementById("horeca").innerHTML += key + " " + boodschappen[key]+"<br>";
-}
+let totaal_bier = drank.bier.amount * drank.bier.price
+let totaal_wijn = drank.wijn.amount * drank.wijn.price
+let totaal_fris = drank.fris.amount * drank.fris.price
+
+let eindbedrag = Math.round(totaal_bier + totaal_wijn + totaal_fris)
+for (key in drank) {
+    console.log(drank[key])
+    document.getElementById("horeca").innerHTML += key + " " + drank[key].name + ": " + drank[key].amount + " x " + drank[key].price.toFixed(2) + " euro" + "<br>";
+
+  }
+document.getElementById("horeca").innerHTML += "Eindbedrag: " + eindbedrag + " euro";
